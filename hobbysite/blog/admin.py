06c5486cb5d66ea768abb14e872af6e7b3ef6238
Paolo <paolo.gines@gmail.com>
2025-03-12 23:26:31 +0800

    Registered admin classes for the models

--- hobbysite/blog/admin.py
+++ hobbysite/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import ArticleCategory, Article
 
 # Register your models here.
+
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    model = ArticleCategory
+
+class ArticleAdmin(admin.ModelAdmin):
+    model = Article
+
+admin.site.register(ArticleCategory, ArticleCategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
