2ca33d0d2fb8bdf519f67802b8a345cedaebd025
Paolo <paolo.gines@gmail.com>
2025-03-12 23:00:44 +0800

    Set article category to be nullable

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -8,8 +8,7 @@ class ArticleCategory(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
-    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL)
+    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True)
     entry = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
-    
