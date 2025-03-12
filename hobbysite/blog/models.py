eb9bdd359541ae4ca429eed10a84550581e3f4b5
Paolo <paolo.gines@gmail.com>
2025-03-12 23:33:37 +0800

    Changed plural name of ArticleCategory

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -6,6 +6,10 @@ class ArticleCategory(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
+    class Meta:
+        verbose_name = "Article Category"
+        verbose_name_plural = "Article Categories"
+
 class Article(models.Model):
     title = models.CharField(max_length=255)
     category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True)
