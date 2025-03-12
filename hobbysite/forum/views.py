b2ffcb79f32662ffd3cc261a1c4f0719931b39ab
pris-matic <deocadizjranthony@gmail.com>
2025-03-12 15:54:14 +0800

    gets categories and threads in database

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import PostCategory
 
 # Create your views here.
+
+def getForumCategories(request):
+    items = PostCategory.objects.prefetch_related('post_set').all()
+    return render(request,"threads.html", {'categories': items})
+
+def getForumThread(request,num):
+    return render(request,"specificThread.html",{'num':num})
