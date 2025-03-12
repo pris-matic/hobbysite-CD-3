4cc97df80ab112681fe6ebf024e605f07c6fdd45
pris-matic <deocadizjranthony@gmail.com>
2025-03-12 15:54:36 +0800

    added forum urls

--- hobbysite/forum/urls.py
+++ hobbysite/forum/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from .views import getForumCategories, getForumThread
 
 urlpatterns = [
-
+    path('forum/threads/',getForumCategories, name= 'getForumCategories'),
+    path('forum/thread/<int:num>/',getForumThread, name='getForumThread'),
 ]
 
 app_name = 'forum'
