41625ee5d07017b2c9663186f016da6f08030f6f
Bien Zuniga <bien.zuniga@student.ateneo.edu>
2025-03-12 19:23:39 +0800

    Updated search and filter fields

--- hobbysite/wiki/admin.py
+++ hobbysite/wiki/admin.py
@@ -9,15 +9,15 @@ class ArticleInline(admin.StackedInline):
 
 class ArticleCategoryAdmin(admin.ModelAdmin):
     model = ArticleCategory
-    search_fields = ("name",)
+    search_fields = ("title",)
     inlines = [
             ArticleInline,
             ]
 
 class ArticleAdmin(admin.ModelAdmin):
     model = Article
-    search_fields = ("name", "category", "entry")
-    list_filter = ("name","category",)
+    search_fields = ("title", "category", "entry")
+    list_filter = ("title","category",)
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(ArticleCategory, ArticleCategoryAdmin)
