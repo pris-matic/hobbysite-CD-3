87757cef00c862d4b7ce64e314cdf837ba477a84
pris-matic <deocadizjranthony@gmail.com>
2025-03-13 00:35:19 +0800

    now required for user to log in

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from .models import PostCategory
+from django.contrib.auth.decorators import login_required
+from .models import PostCategory, Post
 
 # Create your views here.
 
+# @login_required
 def getForumCategories(request):
     items = PostCategory.objects.prefetch_related('post_set').all()
     return render(request,"threads.html", {'categories': items})
 
 def getForumThread(request,num):
-    return render(request,"specificThread.html",{'num':num})
+    post = Post.objects.get(id=num)
+    return render(request,"specificThread.html",{'thread': post})
