f13176e2e2afca96b1df1549a48f278fe3d95170
Bien Zuniga <bien.zuniga@student.ateneo.edu>
2025-03-12 17:28:49 +0800

    created placeholder views
    Wiki articles list and article detail view

--- hobbysite/wiki/urls.py
+++ hobbysite/wiki/urls.py
@@ -1,1 +1,7 @@
+from django.urls import path
+from .views import articles_list, article_detail
 
+urlpatterns = [
+        path('wiki/articles/', articles_list, name='articles_list'),
+        path('wiki/article/<int:pk>/', article_detail, name='article_detail'),
+        ]
--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def articles_list(request):
+    return HttpResponse("Hello World. Wiki article list will go here")
+
+def article_detail(request, pk = 1):
+    return HttpResponse("Hello World. Wiki article detail will go here")
