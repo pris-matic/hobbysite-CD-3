8462680f2c028febb5a180788254c114128fa748
pris-matic <deocadizjranthony@gmail.com>
2025-03-12 15:55:35 +0800

    registered models for categories and posts

--- hobbysite/forum/admin.py
+++ hobbysite/forum/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import PostCategory, Post
 
 # Register your models here.
+
+class PostInline(admin.TabularInline):
+    model = Post
+    extra = 0
+    fields = ('title', 'createdOn', 'updatedOn') 
+    readonly_fields = ('createdOn', 'updatedOn')  
+
+class PostCategoryAdmin(admin.ModelAdmin):
+    model = PostCategory
+
+    list_display = ('name',)
+    list_filter = ('name',)
+    search_fields =('name',)
+    inlines = [PostInline]
+
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+
+    list_display = ('title', 'category', 'createdOn', 'updatedOn',)
+    list_filter = ('category',)
+    search_fields =('category__name',)
+
+admin.site.register(PostCategory,PostCategoryAdmin)
+admin.site.register(Post,PostAdmin)
