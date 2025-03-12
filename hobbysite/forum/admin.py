5c9d39a4a236debb2ef57bb4855590c457be82df
pris-matic <deocadizjranthony@gmail.com>
2025-03-13 00:35:57 +0800

    added comment admin panel

--- hobbysite/forum/admin.py
+++ hobbysite/forum/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import PostCategory, Post
+from django.contrib.auth.models import User
+from .models import PostCategory, Post, Profile, Comment
 
 # Register your models here.
 
@@ -23,6 +24,25 @@ class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'createdOn', 'updatedOn',)
     list_filter = ('category',)
     search_fields =('category__name',)
+    readonly_fields = ('createdOn', 'updatedOn') 
+
+class ProfileInline(admin.StackedInline):
+	model = Profile
+	can_delete = False
+
+class UserAdmin(admin.ModelAdmin):
+	inlines = [ProfileInline]
+      
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
+
+    list_display = ('author', 'post', 'createdOn',)
+    list_filter = ('author', 'post',)
+    readonly_fields = ('createdOn',)
 
 admin.site.register(PostCategory,PostCategoryAdmin)
 admin.site.register(Post,PostAdmin)
+admin.site.register(Comment,CommentAdmin)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
