26f4676d77301bdb6cffbcba7034920ec47e81a4
Bien Zuniga <bien.zuniga@student.ateneo.edu>
2025-03-12 19:14:16 +0800

    created admin panels for 'Article' and 'ArticleCategory'

--- hobbysite/wiki/admin.py
+++ hobbysite/wiki/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Article, ArticleCategory
 
 # Register your models here.
+
+class ArticleInline(admin.StackedInline):
+    model = Article
+    fields = ['title','category','entry']
+
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    model = ArticleCategory
+    search_fields = ("name",)
+    inlines = [
+            ArticleInline,
+            ]
+
+class ArticleAdmin(admin.ModelAdmin):
+    model = Article
+    search_fields = ("name", "category", "entry")
+    list_filter = ("name","category",)
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(ArticleCategory, ArticleCategoryAdmin)
