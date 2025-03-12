c594577710f2247c8d7d3579f68c5c4aa848da0e
Paolo <paolo.gines@gmail.com>
2025-03-12 22:55:20 +0800

    Created Article model

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -5,3 +5,11 @@ from django.db import models
 class ArticleCategory(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL)
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    
