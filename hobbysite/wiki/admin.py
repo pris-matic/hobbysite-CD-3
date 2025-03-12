055e197348f947a7514a2a05061414bd29527d04
Bien Zuniga <bien.zuniga@student.ateneo.edu>
2025-03-12 19:53:58 +0800

    Updated list displays and filters

--- hobbysite/wiki/admin.py
+++ hobbysite/wiki/admin.py
@@ -3,13 +3,14 @@ from .models import Article, ArticleCategory
 
 # Register your models here.
 
-class ArticleInline(admin.StackedInline):
+class ArticleInline(admin.TabularInline):
     model = Article
-    fields = ['title','category','entry']
+    fields = ['title','category']
 
 class ArticleCategoryAdmin(admin.ModelAdmin):
     model = ArticleCategory
-    search_fields = ('title',)
+    search_fields = ('name',)
+    list_display = ('name', 'description')
     inlines = [
             ArticleInline,
             ]
@@ -17,8 +18,8 @@ class ArticleCategoryAdmin(admin.ModelAdmin):
 class ArticleAdmin(admin.ModelAdmin):
     model = Article
     search_fields = ('title', 'category', 'entry')
-    list_filter = ('title','category',)
-    list_display = ('title', 'created_on', 'updated_on')
+    list_filter = ('category',)
+    list_display = ('title', 'category', 'created_on', 'updated_on')
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(ArticleCategory, ArticleCategoryAdmin)
