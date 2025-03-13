7ab0a5fc795654d38932ede181730a7069458a3f
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-03-13 14:16:47 +0800

    Registered Commission and Comment to admin

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Commission, Comment
 
 # Register your models here.
+
+class CommissionAdmin(admin.ModelAdmin):
+    model = Commission
+
+class CommentAdmin(admin.ModelAdmin):
+    model = Comment
+
+admin.site.register(Commission, CommentAdmin)
+admin.site.register(Comment, CommentAdmin)
