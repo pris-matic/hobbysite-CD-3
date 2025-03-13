0a90447787f45a178e4bbdb4411665cc2cec0dba
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-03-14 00:44:34 +0800

    Modified urls.py in commissions

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import commissions_list, commission_detail
 
 urlpatterns = [
-    path('/commissions/list', commissions_list, name='commissions_list'),
-    path('/commissions/detail/1', commission_detail, name='commission_detail'),
+    path('/list', commissions_list, name='commissions_list'),
+    path('/detail/<int:pk>', commission_detail, name='commission_detail'),
 ]
 
 app_name = "commissions"
