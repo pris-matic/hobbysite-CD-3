7333976bd27fa99612be297b99260c27c185ccb9
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-03-14 01:10:51 +0800

    Added more fields for CommissionAdmin and CommentAdmin

--- hobbysite/commissions/admin.py
+++ hobbysite/commissions/admin.py
@@ -5,9 +5,15 @@ from .models import Commission, Comment
 
 class CommissionAdmin(admin.ModelAdmin):
     model = Commission
+    list_display = ('title', 'description', 'people_required', 'created_on', 'updated_on')
+    search_fields = ('title', 'description')
+    ordering = ('created_on',)
 
 class CommentAdmin(admin.ModelAdmin):
     model = Comment
+    list_display = ('commission', 'entry', 'created_on', 'updated_on')
+    search_fields = ('commission', 'entry')
+    ordering = ('-created_on',)
 
-admin.site.register(Commission, CommentAdmin)
+admin.site.register(Commission, CommissionAdmin)
 admin.site.register(Comment, CommentAdmin)
