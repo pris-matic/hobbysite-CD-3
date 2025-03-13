c9c48215cf5b3279217d38179a72ff1761237776
Keithlog <161996366+Keithlog@users.noreply.github.com>
2025-03-13 20:07:29 +0800

    added view functions

--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -1,3 +1,10 @@
+from .models import ProductType, Product
 from django.shortcuts import render
 
-# Create your views here.
+def items_list(request):
+    items = ProductType.objects.all()
+    return render(request,"items_list.html", {'items': items})
+
+def item_details(request, id):
+    item = Product.objects.get(id=id)
+    return render(request,"item_details.html", {'item': item})
