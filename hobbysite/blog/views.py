6b66d707af91249aefac68494423c2fcf2bb80c1
Paolo <paolo.gines@gmail.com>
2025-03-13 21:26:25 +0800

    Created template for list of ArticleCategories

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import ArticleCategory
 
 # Create your views here.
+
+def article_categories_list(request):
+    categories = ArticleCategory.objects.all()
+    return render(request, 'categories_list.html', {'categories': categories})
