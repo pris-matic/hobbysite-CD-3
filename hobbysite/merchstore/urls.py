fdfb954294411ee09f2a08b14371110c5bd89d4c
Keithlog <161996366+Keithlog@users.noreply.github.com>
2025-03-13 19:29:20 +0800

    added ProductType and Product models

--- hobbysite/merchstore/urls.py
+++ hobbysite/merchstore/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('items/', views.items_list, name='items_list'),
-    path('item/<int:item_id>/', views.item_details, name='item_details'),
+    path('item/<int:id>/', views.item_details, name='item_details'),
 ]
