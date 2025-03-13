708fbe276c9644d5a094f8307a3bf8363561f85a
Paolo <paolo.gines@gmail.com>
2025-03-13 21:38:30 +0800

    Registered blog urls and created path for ArticleCategories

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from .views import article_categories_list
 
 urlpatterns = [
-    
+    path('blog/categories/', article_categories_list, name='blog_categories'),
 ]
 
 app_name = 'blog'
