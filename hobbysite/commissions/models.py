ff118f3020f0b01fd86a49751a894b7f85945bab
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-03-13 13:55:39 +0800

    Created a Commission class with its required fields

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Commission(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    people_required = models.IntegerField()
+    created_on = models.DateField(auto_now_add=True)
+    updated_on = models.DateField(auto_now=True)
+
+    class Meta:
+        ordering = ['created_on']
+
