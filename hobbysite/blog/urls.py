07f47b6c9c44b1f51e0821cdcd410d55609fab31
Paolo <paolo.gines@gmail.com>
2025-03-13 21:42:37 +0800

    Renamed path for blog list view

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import article_categories_list
 
 urlpatterns = [
-    path('blog/categories/', article_categories_list, name='blog_categories'),
+    path('blog/articles/', article_categories_list, name='blog_categories'),
 ]
 
 app_name = 'blog'
