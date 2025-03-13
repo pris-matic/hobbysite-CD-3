513973fa6d2e288a8ca4e0a5d9c092f4b5310e00
Paolo <paolo.gines@gmail.com>
2025-03-13 11:56:49 +0800

    Implemented admin customization

--- hobbysite/blog/admin.py
+++ hobbysite/blog/admin.py
@@ -5,9 +5,15 @@ from .models import ArticleCategory, Article
 
 class ArticleCategoryAdmin(admin.ModelAdmin):
     model = ArticleCategory
+    list_display = ('name', 'description',)
+    search_fields = ('name',)
+    list_filter = ('name',)
 
 class ArticleAdmin(admin.ModelAdmin):
     model = Article
+    list_display = ('title', 'category', 'created_on', 'updated_on')
+    search_fields = ('title',)
+    list_filter = ('category', 'created_on', 'updated_on')
 
 admin.site.register(ArticleCategory, ArticleCategoryAdmin)
 admin.site.register(Article, ArticleAdmin)
