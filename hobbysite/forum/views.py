0df7d5fad0c79867abe873d114cdffa7e4f64eb1
pris-matic <deocadizjranthony@gmail.com>
2025-03-13 16:41:35 +0800

    changed method naming to snake_case and removed login requirement

--- hobbysite/forum/views.py
+++ hobbysite/forum/views.py
@@ -1,14 +1,12 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from .models import PostCategory, Post
 
 # Create your views here.
 
-# @login_required
-def getForumCategories(request):
+def get_forum_categories(request):
     items = PostCategory.objects.prefetch_related('post_set').all()
     return render(request,"threads.html", {'categories': items})
 
-def getForumThread(request,num):
+def get_forum_thread(request,num):
     post = Post.objects.get(id=num)
     return render(request,"specificThread.html",{'thread': post})
