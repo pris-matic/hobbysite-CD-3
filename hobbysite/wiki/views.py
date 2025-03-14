0f6bbea0db0199a07f58e76182d4cb12ca88e291
Bien Zuniga <bien.zuniga@student.ateneo.edu>
2025-03-14 10:46:59 +0800

    Updated articles_list view

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Article, ArticleCategory
 
 # Create your views here.
 
 def articles_list(request):
-    return HttpResponse("Hello World. Wiki article list will go here")
+    categories = ArticleCategory.objects.all()
+    articles = Article.objects.all()
+    return render(request, 'wiki/articles_list.html', {'categories':categories, 'articles':articles})
 
-def article_detail(request, pk = 1):
-    return HttpResponse("Hello World. Wiki article detail will go here")
+def article_detail(request, key):
+    article = Article.objects.get(id=key)
+    return render(request, '', {'article': article})
