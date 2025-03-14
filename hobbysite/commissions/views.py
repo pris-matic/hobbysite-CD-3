ad0134c895c7745fa4a5158555db2752e88aa2b3
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-03-14 18:12:13 +0800

    Modified template names to reflect recent renaming of html templates

--- hobbysite/commissions/views.py
+++ hobbysite/commissions/views.py
@@ -6,8 +6,8 @@ from .models import Commission
 def commissions_list(request):
     commissions = Commission.objects.all()
     ctx = {'commissions': commissions}
-    return render(request, 'commissions_list.html', ctx)
+    return render(request, 'commissionsList.html', ctx)
 
 def commission_detail(request, id):
     ctx = {'commission': Commission.objects.get(id=id)}
-    return render(request, 'commission_detail.html', ctx)
+    return render(request, 'commissionDetail.html', ctx)
