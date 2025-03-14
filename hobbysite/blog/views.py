c3acaaf05130ecf9324ca884f2b4b6f894eedfff
Paolo <paolo.gines@gmail.com>
2025-03-14 09:57:38 +0800

    Renamed template files for clarity

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -5,8 +5,8 @@ from .models import ArticleCategory, Article
 
 def article_categories_list(request):
     categories = ArticleCategory.objects.all()
-    return render(request, 'categories_list.html', {'categories': categories})
+    return render(request, 'blogArticlesList.html', {'categories': categories})
 
 def article(request, key):
     article = Article.objects.get(id=key)
-    return render(request, 'article.html', {'article': article})
+    return render(request, 'blogArticle.html', {'article': article})
