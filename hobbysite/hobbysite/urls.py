6435a3daec9ba25b22ccb961bbdc255b3ba0ad7c
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-03-14 08:46:35 +0800

    Removed commissions in the path route

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('commissions/', include('commissions.urls'))
+    path('', include('commissions.urls'))
 ]
