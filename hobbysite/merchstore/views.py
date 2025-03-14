bef73f6ba752777401f19660f9685957faf8cbf5
Keithlog <161996366+Keithlog@users.noreply.github.com>
2025-03-14 21:40:48 +0800

    fixed products not showing error in view functions

--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -6,5 +6,5 @@ def items_list(request):
     return render(request,"itemsList.html", {'items': items})
 
 def item_details(request, item_id):
-    item = Product.objects.get(id=item_id)
+    item = ProductType.objects.get(id=item_id)
     return render(request,"itemDetails.html", {'item': item})
