a25521d130e3d0b7047e8e77dcbc2223ddca92d4
pris-matic <deocadizjranthony@gmail.com>
2025-04-27 03:16:17 +0800

    allows editing of name and email

--- hobbysite/user_management/views.py
+++ hobbysite/user_management/views.py
@@ -1,2 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Profile
+from .forms import ProfileForm
 
+@login_required
+def update_profile(request):
+    account, created = Profile.objects.get_or_create(user=request.user)
+
+    if request.method == 'POST':
+        profileForm = ProfileForm(request.POST, instance=account)
+
+        if profileForm.is_valid():
+            profile = profileForm.save(commit=False)
+            profile.user = request.user
+            profile.save()
+            return redirect('user_management:update_profile')
+        
+    else:
+        profileForm = ProfileForm(instance=account)
+
+    return render(request, 'user_management/profile.html', {'account': account , 'profile': profileForm})
