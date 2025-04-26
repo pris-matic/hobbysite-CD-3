dcde1f197dd58de7f114aa6b48e3af97b56ca993
pris-matic <deocadizjranthony@gmail.com>
2025-04-27 03:15:55 +0800

    added custom user model and form

--- hobbysite/user_management/models.py
+++ hobbysite/user_management/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=63)
     email = models.EmailField()
 
     def __str__(self):
