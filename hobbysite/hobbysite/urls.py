8144da7af116b76b736fc8c197cb9ef089b7760b
pris-matic <deocadizjranthony@gmail.com>
2025-05-01 11:25:09 +0800

    changed path of user_management to '' instead

--- hobbysite/hobbysite/urls.py
+++ hobbysite/hobbysite/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     path('commissions/', include('commissions.urls')),
     path('forum/', include('forum.urls')),
     path('wiki/', include('wiki.urls')),   
-    path('profile/',include('user_management.urls')),
+    path('',include('user_management.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
