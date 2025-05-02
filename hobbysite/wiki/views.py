656266fa27d49948302894c141e3968cb2b9975e
Bien Zuniga <bien.zuniga@student.ateneo.edu>
2025-05-02 19:52:33 +0800

    Updated article list view and template to separate current user's articles

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -3,7 +3,11 @@ from .models import Article, ArticleCategory
 
 def articles_list(request):
     categories = ArticleCategory.objects.all()
-    return render(request, 'wiki/articlesList.html', {'categories':categories})
+    if request.user.is_authenticated:
+        user_articles = Article.objects.filter(author__user=request.user)
+    else: 
+        user_articles = {}
+    return render(request, 'wiki/articlesList.html', {'categories':categories, 'user_articles':user_articles})
 
 def article_detail(request, key):
     return render(request, 'wiki/articleDetail.html', {'article': Article.objects.get(id=key)})
