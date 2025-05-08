2cb5c541dbd405269917cfe949dec245007833ae
Bien Zuniga <bien.zuniga@student.ateneo.edu>
2025-05-08 15:26:46 +0800

    Added article creation form and view

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 from .models import Article, ArticleCategory
+from .forms import ArticleForm
 
 def articles_list(request):
     categories = ArticleCategory.objects.all()
@@ -11,3 +12,17 @@ def articles_list(request):
 
 def article_detail(request, key):
     return render(request, 'wiki/articleDetail.html', {'article': Article.objects.get(id=key)})
+
+def article_create(request):
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST, request.FILES)
+        if article_form.is_valid():
+            article_form.instance.author = request.user.profile
+            article = article_form.save()
+            return redirect(reverse('wiki:article_detail', args=[article.pk]))
+        else:
+            print(article_form.errors)
+    else: 
+        article_form = ArticleForm()
+    return render(request, 'wiki/articleCreate.html', {'article_form': article_form})
+    
