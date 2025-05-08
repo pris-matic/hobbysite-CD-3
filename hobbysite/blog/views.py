6da12f3e0bd54038808fe5f11e7cd98bae1e8f5b
Paolo <paolo.gines@gmail.com>
2025-05-08 21:14:31 +0800

    Implemented required login for article list view

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import ArticleCategory, Article
+from django.contrib.auth.decorators import login_required
 
+@login_required
 def article_categories_list(request):
     categories = ArticleCategory.objects.all()
     return render(request, 'blog/blogArticlesList.html', {'categories': categories})
