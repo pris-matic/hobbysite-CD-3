5a87f8e231a16cf20666051f23f74c30bd80ba13
Bien Zuniga <bien.zuniga@student.ateneo.edu>
2025-05-08 15:59:54 +0800

    added article update view and url

--- hobbysite/wiki/views.py
+++ hobbysite/wiki/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import Article, ArticleCategory
 from .forms import ArticleForm
 
@@ -25,4 +25,17 @@ def article_create(request):
     else: 
         article_form = ArticleForm()
     return render(request, 'wiki/articleCreate.html', {'article_form': article_form})
+
+def article_update(request, key):
+    article = get_object_or_404(Article, id=key)
+    if request.method == 'POST':
+        article_form = ArticleForm(request.POST, request.FILES, instance=article)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect(reverse('wiki:article_detail', args=[article.pk]))
+    else:
+        article_form = ArticleForm(instance=article)
+    
+    return render(request, 'wiki/articleUpdate.html', {'article_form': article_form})
+
     
