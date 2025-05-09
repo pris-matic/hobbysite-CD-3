b054cf832aa2cab3a590b4353488a26964953778
Paolo <paolo.gines@gmail.com>
2025-05-09 21:49:16 +0800

    Added url for article creator to use in articles list view

--- hobbysite/blog/urls.py
+++ hobbysite/blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import article_categories_list, article
+from .views import article_categories_list, article, article_create
 
 urlpatterns = [
     path('articles/', article_categories_list, name='blog_categories'),
     path('article/<int:key>/', article, name='article'),
+    path('article/add/', article_create, name='article_create')
 ]
 
 app_name = 'blog'
