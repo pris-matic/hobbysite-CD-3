76193403426dd4cfde87ff9ac9bc9d70ee7ea88d
pris-matic <deocadizjranthony@gmail.com>
2025-05-09 19:44:59 +0800

    added forms for comment, thread, and threadcategory

--- hobbysite/forum/forms.py
+++ hobbysite/forum/forms.py
@@ -1,17 +1,17 @@
 from django import forms
 from .models import ThreadCategory, Thread, Comment
 
-class ThreadCategoryForm(forms.modelForm):
+class ThreadCategoryForm(forms.ModelForm):
     class Meta:
         model = ThreadCategory
         fields = ['name','description']
 
-class ThreadForm(forms.modelForm):
+class ThreadForm(forms.ModelForm):
     class Meta:
         model = Thread
         fields = ['title','category','entry','image']
 
-class CommentForm(forms.modelForm):
+class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['entry']
