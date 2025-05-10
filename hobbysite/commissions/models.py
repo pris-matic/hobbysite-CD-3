8fba24e63518703a33d2b1f6c9e3070548366778
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-05-11 00:40:54 +0800

    edited DateField  in both models to DateTimeField

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -6,8 +6,8 @@ class Commission(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     people_required = models.IntegerField()
-    created_on = models.DateField(auto_now_add=True)
-    updated_on = models.DateField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
@@ -18,8 +18,8 @@ class Commission(models.Model):
 class Comment(models.Model):
     commission = models.ForeignKey(Commission, on_delete=models.CASCADE, related_name='comments')
     entry = models.TextField()
-    created_on = models.DateField(auto_now_add=True)
-    updated_on = models.DateField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f"Comment found in {self.commission.title}, created on {self.created_on.strftime('%d-%m-%Y')}"
