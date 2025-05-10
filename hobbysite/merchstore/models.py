0d6ed6603a7d0b090c3f1fc112991f0c593268cc
Keithlog <161996366+Keithlog@users.noreply.github.com>
2025-05-10 19:00:15 +0800

    updated product model with new fields

--- hobbysite/merchstore/models.py
+++ hobbysite/merchstore/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from user_management.models import Profile
 
 class ProductType(models.Model):
     name = models.CharField(max_length=255)
@@ -12,16 +13,24 @@ class ProductType(models.Model):
         return self.name
 
 class Product(models.Model):
+    STATUS_CHOICES = [
+        ('Available', 'Available'),
+        ('On sale', 'On sale'),
+        ('Out of stock', 'Out of stock'),
+    ]
+        
     name = models.CharField(max_length=255)
-    product_type = models.ForeignKey(ProductType, on_delete=models.SET_NULL, related_name='products', null=True)
+    product_type = models.ForeignKey(ProductType, null=True, on_delete=models.SET_NULL)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Available')
 
-    class Meta:
-        ordering = ['name']
+    def save(self, *args, **kwargs):
+        if self.stock == 0:
+            self.status = 'Out of stock'
+        else:
+            self.status = 'Available'
+        super().save(*args, **kwargs)
     
-    def __str__(self):
-        return self.name
-    
-    def get_absolute_url(self):
-        return reverse('item_details', args=[str(self.id)])
