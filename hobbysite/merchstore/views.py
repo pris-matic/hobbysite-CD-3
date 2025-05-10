0c3e006fe9410b483d1583afcf7690cfef9fd327
Keithlog <161996366+Keithlog@users.noreply.github.com>
2025-05-10 19:56:08 +0800

    updated product list and product detail views

--- hobbysite/merchstore/views.py
+++ hobbysite/merchstore/views.py
@@ -1,10 +1,42 @@
-from .models import ProductType, Product
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
+from .forms import TransactionForm
 
-def items_list(request):
-    items = ProductType.objects.all()
-    return render(request,"merchstore/itemsList.html", {'items': items})
+def product_list_view(request):
+    all_products = Product.objects.all()
+    user_products = []
+    if request.user.is_authenticated:
+        profile = request.user.profile
+        user_products = all_products.filter(owner=profile)
+        all_products = all_products.exclude(owner=profile)
 
-def item_details(request, item_id):
-    item = ProductType.objects.get(id=item_id)
-    return render(request,"merchstore/itemDetails.html", {'item': item})
+    return render(request, 'merchstore/product_list.html', {
+        'user_products': user_products,
+        'all_products': all_products,
+    })
+
+def product_detail_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    form = TransactionForm(request.POST or None)
+    is_owner = request.user.is_authenticated and request.user.profile == product.owner
+
+    if request.method == 'POST' and not is_owner:
+        if not request.user.is_authenticated:
+            return redirect('login')
+
+        if form.is_valid():
+            transaction = form.save(commit=False)
+            transaction.product = product
+            transaction.buyer = request.user.profile
+
+            if product.stock >= transaction.amount:
+                product.stock -= transaction.amount
+                product.save()
+                transaction.save()
+                return redirect('cart')
+
+    return render(request, 'merchstore/product_detail.html', {
+        'product': product,
+        'form': form,
+        'is_owner': is_owner,
+    })
