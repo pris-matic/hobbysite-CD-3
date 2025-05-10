30a85f129c30837db102009c4a97635b3566914c
Paolo <paolo.gines@gmail.com>
2025-05-11 00:55:25 +0800

    Improved style on forms

--- hobbysite/blog/forms.py
+++ hobbysite/blog/forms.py
@@ -5,8 +5,31 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['entry']
+        widgets = {
+            'entry': forms.Textarea(attrs={
+                'rows': 1,
+                'placeholder': 'Write your comment here...',
+                'class': 'form-control'
+            })
+        }
 
 class ArticleCreateForm(forms.ModelForm):
     class Meta:
         model = Article
         fields = ['title', 'category', 'entry', 'header_image']
+        widgets = {
+            'title': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'category': forms.Select(attrs={
+                'class': 'form-control'
+            }),
+            'entry': forms.Textarea(attrs={
+                'rows': 5,
+                'placeholder': 'Write your article here...',
+                'class': 'form-control'
+            }),
+            'header_image': forms.FileInput(attrs={
+                'class': 'form-control'
+            }),
+        }
