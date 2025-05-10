9a7565c7c78253f784667b2204e996c519d04bc0
Keithlog <161996366+Keithlog@users.noreply.github.com>
2025-05-10 19:14:06 +0800

    created forms.py and added product and transaction forms

---
+++ hobbysite/merchstore/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Product, Transaction
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        exclude = ['owner']
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['amount']
