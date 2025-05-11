806a097a58fc8100e658159cfb2b2e1a0b72adb1
pris-matic <deocadizjranthony@gmail.com>
2025-05-11 10:50:59 +0800

    added register and password_reset url

--- hobbysite/user_management/urls.py
+++ hobbysite/user_management/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import update_profile, homepage
+from .views import update_profile, homepage, register, password_reset
 
 urlpatterns = [
     path('profile/',update_profile, name='update_profile'),
     path('home/',homepage ,name='home'),
+    path('register/',register, name='register'),
+    path('reset/', password_reset, name='password_reset'),
 ]
 
 app_name = 'user_management'
