afea304d1bcd2141d5705cdcdd4d27ffabe62a80
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-05-11 16:32:09 +0800

    replaced Comments to Job and added its appropriate fields

--- hobbysite/commissions/models.py
+++ hobbysite/commissions/models.py
@@ -21,14 +21,19 @@ class Commission(models.Model):
     class Meta:
         ordering = ['created_on']
 
-class Comment(models.Model):
-    commission = models.ForeignKey(Commission, on_delete=models.CASCADE, related_name='comments')
-    entry = models.TextField()
-    created_on = models.DateTimeField(auto_now_add=True)
-    updated_on = models.DateTimeField(auto_now=True)
+class Job(models.Model):
+    STATUS_STATES = [
+        ('Open', 'Open'),
+        ('Full', 'Full'),
+    ]
+
+    commission = models.ForeignKey(Commission, on_delete=models.CASCADE, related_name='jobs')
+    role = models.CharField(max_length=255)
+    manpower_required = models.PositiveIntegerField()
+    status = models.CharField(max_length=20, choices=STATUS_STATES, default='Open')
 
     def __str__(self):
-        return f"Comment found in {self.commission.title}, created on {self.created_on.strftime('%d-%m-%Y')}"
+        return f"{self.role} ({self.status})"
 
     class Meta:
         ordering = ['-created_on']
