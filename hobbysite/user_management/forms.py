4d29d9af6479a1be67adbf8c61f640f3a1b6c5a9
pris-matic <deocadizjranthony@gmail.com>
2025-05-11 10:50:43 +0800

    added name field

--- hobbysite/user_management/forms.py
+++ hobbysite/user_management/forms.py
@@ -1,7 +1,17 @@
 from django import forms
 from .models import Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['name', 'email']
+
+class UserRegisterForm(UserCreationForm):
+    name = forms.CharField(max_length=63, required=True)
+    email = forms.EmailField(required=True)
+    
+    class Meta:
+        model = User
+        fields = ['username','name','password1', 'password2','email']
