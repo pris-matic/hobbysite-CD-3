fba6490a39bb63ba2022a85d9001d0591e2a8567
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-05-12 11:10:04 +0800

    added a url for creating new commissions

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import commissions_list, commission_detail
+from .views import commissions_list, commission_detail, commission_create_view
 
 urlpatterns = [
     path('list/', commissions_list, name='commissions_list'),
     path('detail/<int:id>/', commission_detail, name='commission_detail'),
+    path('add/', commission_create_view, name='commission_create'),
 ]
 
 app_name = "commissions"
