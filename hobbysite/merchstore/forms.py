74c88d89160c04ba7a45dc950ed74d876df97aa8
Keithlog <161996366+Keithlog@users.noreply.github.com>
2025-05-12 19:46:06 +0800

    added form widgets for better layout

--- hobbysite/merchstore/forms.py
+++ hobbysite/merchstore/forms.py
@@ -6,8 +6,35 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = ['name', 'product_type', 'description', 'price', 'stock', 'status']
         exclude = ['owner']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'class': 'form-control'
+            }),
+            'product_type': forms.Select(attrs={
+                'class': 'form-control'
+            }),
+            'description': forms.Textarea(attrs={
+                'rows': 3,
+                'placeholder': 'Describe your product...',
+                'class': 'form-control'
+            }),
+            'price': forms.NumberInput(attrs={
+                'class': 'form-control'
+            }),
+            'stock': forms.NumberInput(attrs={
+                'class': 'form-control'
+            }),
+            'status': forms.Select(attrs={
+                'class': 'form-control'
+            })
+        }
 
 class TransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
         fields = ['amount']
+        widgets = {
+            'amount': forms.NumberInput(attrs={
+                'class': 'form-control'
+            })
+        }
