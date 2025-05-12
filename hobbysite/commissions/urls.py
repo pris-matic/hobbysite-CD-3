14479780e8417fbe946a3b882f78813bd875e90a
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-05-12 11:15:45 +0800

    renamed view to commission_create for consistency with other views name (edited in urls.py)

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import commissions_list, commission_detail, commission_create_view
+from .views import commissions_list, commission_detail, commission_create
 
 urlpatterns = [
     path('list/', commissions_list, name='commissions_list'),
     path('detail/<int:id>/', commission_detail, name='commission_detail'),
-    path('add/', commission_create_view, name='commission_create'),
+    path('add/', commission_create, name='commission_create'),
 ]
 
 app_name = "commissions"
