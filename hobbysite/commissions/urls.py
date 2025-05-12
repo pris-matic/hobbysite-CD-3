82bcb3f5eede06d1d67de9799683388c4da7a187
ejjaso18 <164835349+ejjaso18@users.noreply.github.com>
2025-05-12 11:41:09 +0800

    added the url path for commission update

--- hobbysite/commissions/urls.py
+++ hobbysite/commissions/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import commissions_list, commission_detail, commission_create
+from .views import commissions_list, commission_detail, commission_create, commission_update
 
 urlpatterns = [
     path('list/', commissions_list, name='commissions_list'),
     path('detail/<int:id>/', commission_detail, name='commission_detail'),
     path('add/', commission_create, name='commission_create'),
+    path('<int:id>/edit/', commission_update, name='commission_update'),
 ]
 
 app_name = "commissions"
